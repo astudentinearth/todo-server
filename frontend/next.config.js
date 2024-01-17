ff6f4c0c02bd851f1f0a3770df460144a74825dc
astudentinearth <burak.yn.dev@gmail.com>
2024-01-18 02:20:23 +0300

    🔨 Update build configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true
+    },
     async rewrites(){
         return [{
             source: "/api/v1/:path*",
