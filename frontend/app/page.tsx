a230696a6ef32318342e613dfdd91183f9216ef2
astudentinearth <burak.yn.dev@gmail.com>
2024-01-18 21:31:11 +0300

    🐛 Fix UI rendering without authentication

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,27 +2,27 @@
 
 import Nav from "@/components/nav";
 import { TodoList } from "@/components/todoList";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
-
+  const [auth, setAuth] = useState(false);
   useEffect(()=>{
     const req = new XMLHttpRequest();
     req.onload = ()=>{
         if(req.status!=200){
             window.location.href="/login";
         }
+        else setAuth(true);
     };
     req.open("GET","/api/v1/validate-session",true);
     req.send();
   },[])
-  return (
+  return ( auth ?
     <>
       <Nav></Nav>
       <div className="flex flex-col items-center">
-        
         <TodoList></TodoList>
-    </div>
-    </>
+      </div>
+    </> : <></>
   )
 }
