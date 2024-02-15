86daa4c5705a9899649345efc29a4f6307e708b5
astudentinearth <burak.yn.dev@gmail.com>
2024-02-15 21:36:32 +0300

    💄 Update signup ui - Update validation logic - Add error text - Make the UI more responsive

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -6,7 +6,7 @@ import { getUser } from "@/lib/actions/auth.actions";
 export default async function SignupPage(){
     const user = await getUser();
     if(user) redirect("/");
-    return <div className="absolute left-[50%] top-[50%] bg-black-2 translate-x-[-50%] translate-y-[-50%] text-white p-4 rounded-lg drop-shadow-xl border-[1px] border-widget-normal hover:border-widget-hover">
+    return <div className="absolute sm:left-[50%] sm:right-auto sm:top-[50%] bottom-0 sm:bottom-auto justify-center bg-black-1 sm:bg-black-2 sm:translate-x-[-50%] sm:w-[360px] w-full h-full sm:h-auto flex flex-col sm:translate-y-[-50%] text-white p-4 sm:rounded-lg sm:drop-shadow-xl sm:border-[1px] border-widget-normal hover:border-widget-hover">
         <h1 className="select-none">Create account</h1>
         <SignupForm></SignupForm>
         <span className="block text-lg mt-4">{"Already have an account? "} <Link className="text-primary hover:underline" href={"/login"}>Sign in</Link></span>
--- src/components/forms/SignupForm.tsx
+++ src/components/forms/SignupForm.tsx
@@ -12,6 +12,7 @@ export default function SignupForm(){
     const confirmPasswordRef = useRef<HTMLInputElement>(null)
     const [working, setWorking] = useState(false);
     const [formValid, setFormValid] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
     const handleClick = async ()=>{
         if(usernameRef.current==null || passwordRef.current==null || confirmPasswordRef.current==null) return;
         if(usernameRef.current.value.trim().length==0){ alert("Provide an username"); setWorking(false); return}
@@ -26,22 +27,24 @@ export default function SignupForm(){
     }
     const validate = ()=>{
         if(usernameRef.current==null || passwordRef.current==null || confirmPasswordRef.current==null) return;
-        if(usernameRef.current.value.trim().length==0){ setFormValid(false); return}
-        if(usernameRef.current.value.trim().length > 64) {setFormValid(false); return}
-        if(!/^[a-zA-Z0-9_]+$/.test(usernameRef.current.value)) {setFormValid(false); return}
-        if(passwordRef.current.value.length==0){ setFormValid(false); return}
-        if(passwordRef.current.value.length<8){ setFormValid(false); return}
-        if(passwordRef.current.value.length>64){ setFormValid(false); return}
-        if(confirmPasswordRef.current.value!==passwordRef.current.value) {setFormValid(false); return}
-        if(confirmPasswordRef.current.value!==passwordRef.current.value) {setFormValid(false); return}
+        if(usernameRef.current.value.trim().length==0){setFormValid(false); return}
+        if(usernameRef.current.value.trim().length > 64) {setErrorMessage("Usernames must not exceed 64 characters."); setFormValid(false); return}
+        if(!/^[a-zA-Z0-9_]+$/.test(usernameRef.current.value)) {setErrorMessage("Only letters, numbers and underscores are allowed."); setFormValid(false); return}
+        if(passwordRef.current.value.length==0){setErrorMessage(""); setFormValid(false); return}
+        if(passwordRef.current.value.length<8){ setErrorMessage("Password must be at least 8 characters long."); setFormValid(false); return}
+        if(passwordRef.current.value.length>64){setErrorMessage("Password must be at most 64 characters long."); setFormValid(false); return}
+        if(confirmPasswordRef.current.value!==passwordRef.current.value) {setErrorMessage("Passwords don't match.");setFormValid(false); return}
+        if(usernameRef.current.value === passwordRef.current.value) {setErrorMessage("Password and username can't be the same."); setFormValid(false)}
+        setErrorMessage("");
         setFormValid(true);
     }
     useEffect(()=>{setFormValid(false)},[]);
     return <form>
-        <TextInput onChange={validate} required inputRef={usernameRef} className="w-72 mt-4 text-lg p-2 bg-black-2" placeholder="Username"></TextInput>
-        <TextInput onChange={validate} required inputRef={passwordRef} type="password" className="w-72 mt-4 text-lg p-2 bg-black-2" placeholder="Password"></TextInput>
-        <TextInput onChange={validate} required inputRef={confirmPasswordRef} type="password" className="w-72 mt-4 text-lg p-2 bg-black-2" placeholder="Confirm password"></TextInput>
-        <Button onClick={handleClick} type="submit" disabled={working || !formValid} className="block mt-4 text-center w-72 text-lg p-2">
+        <TextInput onChange={validate} required inputRef={usernameRef} className="w-full mt-4 text-lg p-2 bg-black-2" placeholder="Username"></TextInput>
+        <TextInput onChange={validate} required inputRef={passwordRef} type="password" className="w-full mt-4 text-lg p-2 bg-black-2" placeholder="Password"></TextInput>
+        <TextInput onChange={validate} required inputRef={confirmPasswordRef} type="password" className="w-full mt-4 text-lg p-2 bg-black-2" placeholder="Confirm password"></TextInput>
+        {errorMessage ? <span className=" text-[12px] text-red-500 text-ellipsis">{errorMessage}</span> : <span className="text-base">&nbsp;</span>}
+        <Button onClick={handleClick} type="submit" disabled={working || !formValid} className="block mt-4 text-center w-full text-lg p-2">
             {working ? <span className="text-center translate-x-[-24px]"><Image className="animate-spin inline-block" width={24} height={24} src="/loader.svg" alt="Signing in"></Image></span> : "Create account"}
         </Button>
     </form>
